e197ff1f02b94447f8bfe1f519b285dbbe7bd35e
Vallcrist <jefferson@bitcakestudio.com>
2020-08-08 18:05:43 -0300

    Readding samples

---
+++ Editor/Samples/Examples/BuildActions/Scripts/IncrementBundleVersion.cs
@@ -0,0 +1,28 @@
+
+using UnityEditor;
+
+namespace UnityMake
+{
+	public sealed class IncrementBundleVersion : UMakeBuildAction
+	{
+		public override void Execute( UMake umake, UMakeTarget target )
+		{
+			string bundleVersion = PlayerSettings.bundleVersion;
+
+			if ( target.buildTarget == BuildTarget.Android )
+			{
+				PlayerSettings.Android.bundleVersionCode++;
+			}
+			else if ( target.buildTarget == BuildTarget.iOS )
+			{
+				int buildNumber = 0;
+
+				if ( int.TryParse( PlayerSettings.iOS.buildNumber, out buildNumber ) )
+				{
+					buildNumber++;
+					PlayerSettings.iOS.buildNumber = buildNumber.ToString();
+				}
+			}
+		}
+	}
+}
---
+++ Editor/Samples/Examples/BuildActions/Scripts/IncrementVersionBuildAction.cs
@@ -0,0 +1,34 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace UnityMake
+{
+	public sealed class IncrementVersionBuildAction : UMakeBuildAction
+	{
+		public char separator = '.';
+        public bool updateApplicationVersion = false;
+
+		public override void Execute( UMake umake, UMakeTarget target )
+		{
+			Undo.RecordObject( umake, "UMakeBuildAction" );
+
+			string[] parts = umake.version.Split( separator );
+			int minVersion;
+
+			if( int.TryParse( parts[parts.Length - 1], out minVersion ) )
+			{
+				minVersion += 1;
+				parts[parts.Length - 1] = minVersion.ToString();
+
+				umake.version = string.Join( separator.ToString(), parts );
+			}
+
+			if ( updateApplicationVersion )
+			{
+				PlayerSettings.bundleVersion = umake.version;
+				Debug.Log( "The application is now with version " + Application.version );
+			}
+			EditorUtility.SetDirty( umake );
+		}
+	}
+}
