a984af1eb11cecf259a4790b70c6b58b81811fb0
Kim Kaznowski <kim@doubledashstudios.com>
2020-08-16 00:51:27 -0300

    - Remove WebGL, WSA, remaning only Windows and Android targets + Add UpdateOrIncrementBundleVersion + Add Remove Splash + Add ScripintConfigAction and AndroidScripitingConfigAction + Add ApplicationIdentifierAction

---
+++ Editor/Samples/Examples/BuildActions/Scripts/ScriptingConfigAction.cs
@@ -0,0 +1,26 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace UnityMake
+{
+    public class ScriptingConfigAction : UMakeBuildAction
+    {
+        [SerializeField]
+        private BuildTargetGroup targetGroup;
+        [SerializeField]
+        private ScriptingImplementation scriptingImplementation;
+
+
+        public override void Execute(UMake umake, UMakeTarget target)
+        {
+            PlayerSettings.SetScriptingBackend(targetGroup, scriptingImplementation);
+        }
+
+        private void UpdateIfValid(IUMakeParameterProvider parameters, string key, ref string target)
+        {
+            var val = parameters.GetValue(key);
+            if (!string.IsNullOrEmpty(val))
+                target = val;
+        }
+    }
+}
---
+++ Editor/Samples/Examples/BuildActions/Scripts/UpdateOrIncrementBuildVersionCode.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+using UnityMake;
+
+public class UpdateOrIncrementBuildVersionCode : UMakeBuildAction
+{
+    /// <summary>
+    /// If target VersionCode is Equals 
+    /// </summary>
+    [Tooltip("If targetVersionCode equals or less than 0 (ZERO), this action will ignore the number and will work incrementing the current build version. Remember: CLI Number always has priority")]
+    [SerializeField] private int targetVersionCode = -1;
+
+    public override void Execute(UMake umake, UMakeTarget target)
+    {
+        var scriptableTarget = targetVersionCode.ToString();
+
+        if (targetVersionCode < 0)
+        {
+            if (target.buildTarget == BuildTarget.Android)
+            {
+                PlayerSettings.Android.bundleVersionCode++;
+                scriptableTarget = PlayerSettings.Android.bundleVersionCode.ToString();
+            }
+            else if (target.buildTarget == BuildTarget.iOS)
+            {
+                if (int.TryParse(PlayerSettings.iOS.buildNumber, out int buildNumber))
+                {
+                    buildNumber++;
+                    scriptableTarget = buildNumber.ToString();
+                    PlayerSettings.iOS.buildNumber = buildNumber.ToString();
+                }
+            }
+        }
+
+        string externalVersion = scriptableTarget;
+        umake.Parameters.UpdateIfValid("build.number", ref externalVersion);
+
+        if (!string.Equals(externalVersion, scriptableTarget, StringComparison.CurrentCultureIgnoreCase))
+        {
+            if (int.TryParse(externalVersion, out int b))
+            {
+                PlayerSettings.Android.bundleVersionCode = b;
+            }
+            else if (target.buildTarget == BuildTarget.iOS)
+            {
+                PlayerSettings.iOS.buildNumber = externalVersion;
+            }
+        }
+    }
+}
+
+public static class UMakeParameterProviderExtensions
+{
+    public static void UpdateIfValid(this IUMakeParameterProvider parameters, string key, ref string target)
+    {
+        if (parameters == null)
+            return;
+
+        var val = parameters.GetValue(key);
+        if (!string.IsNullOrEmpty(val))
+            target = val;
+    }
+}
